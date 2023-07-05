6fee7561bc9be6f10a930b0fb1d1d65aef7b5f05
Oruc <112250535+oruccoder@users.noreply.github.com>
2023-07-05 22:09:15 +0400

    Add files via upload

---
+++ weather.app/js/index.js
@@ -0,0 +1,36 @@
+const url = 'https://api.openweathermap.org/data/2.5/';
+const key = 'd145a693dfd90188bd0ce7c76196efde';
+
+const setQuery = (e) => {
+  if (e.keyCode === 13) {
+    getResult(searchBar.value);
+  }
+};
+
+const getResult = (cityName) => {
+  let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=az`;
+  fetch(query)
+    .then(weather => {
+      return weather.json();
+    })
+    .then(displayResult);
+};
+ 
+const displayResult = (result) => {
+  let city = document.querySelector('.city');
+  city.innerText = `${result.name}, ${result.sys.country}`;
+
+  let temp = document.querySelector('.temp');
+  temp.innerText = `${Math.round(result.main.temp)}°C`;
+
+  let desc = document.querySelector('.desc')
+
+  desc.innerText = result.weather[0].description
+
+  let minmax = document.querySelector('.minmax')
+  minmax.innerText = `${Math(result.main.temp_min)}`
+};
+
+const searchBar = document.getElementById("searchBar");
+
+searchBar.addEventListener('keypress', setQuery);
---
+++ weather.app/weather.js
@@ -0,0 +1,2 @@
+const url = 'https://api.openweathermap.org/data/2.5/'
+const key = 'd145a693dfd90188bd0ce7c76196efde'
